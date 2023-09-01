c7787e861c529d2e6095315e931756110aa1a599
Joe Bello <joebello123@proton.me>
2023-09-01 19:44:32 -0400

    wip: theming

---
+++ src/theme.jsx
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material'
+
+const theme = createTheme({
+	spacing: {
+		xs: '.25rem',
+		sm: '.5rem',
+		md: '.75rem',
+		lg: '1rem'
+	}
+})
+
+export default theme
